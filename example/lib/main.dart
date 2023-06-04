cb6fa105012991b44ad8495f461f70f9e1c1e562
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2023-06-04 21:45:52 +0900

    feat: add example codes

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,20 +1,39 @@
-import 'package:flutter/material.dart';
+import 'package:flutterfire_json_converters/flutterfire_json_converters.dart';
+import 'package:json_annotation/json_annotation.dart';
 
-void main() {
-  runApp(const MainApp());
+part 'main.g.dart';
+
+@JsonSerializable()
+class Entity {
+  Entity({
+    required this.name,
+    this.createdAt = const ServerTimestamp(),
+    this.updatedAt = const ServerTimestamp(),
+  });
+
+  /// Connect the generated [_$EntityFromJson] function to the `fromJson`
+  /// factory.
+  factory Entity.fromJson(Map<String, dynamic> json) => _$EntityFromJson(json);
+
+  /// The generated code assumes these values exist in JSON.
+  final String name;
+
+  @sealedTimestampConverter
+  final SealedTimestamp createdAt;
+
+  @alwaysUseServerTimestampSealedTimestampConverter
+  final SealedTimestamp updatedAt;
+
+  /// Connect the generated [_$EntityToJson] function to the `toJson` method.
+  Map<String, dynamic> toJson() => _$EntityToJson(this);
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+void main() {
+  final epoch = DateTime(1970);
+  final entity = Entity(name: 'foo', createdAt: ClientDateTime(epoch));
+  final json = entity.toJson();
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+  print(json['name']); // 'foo'
+  print(json['createdAt']); // Timestamp(seconds=0, nanoseconds=0)
+  print(json['updatedAt']); // FieldValue(Instance of 'MethodChannelFieldValue')
 }
