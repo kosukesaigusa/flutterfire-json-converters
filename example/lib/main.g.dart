f38fd84f4e2c3ea0b03a1926094b3081ef7bd4da
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2023-06-05 14:20:09 +0900

    chore: update comments in entity fields

--- example/lib/main.g.dart
+++ example/lib/main.g.dart
@@ -1,12 +1,14 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
+// ignore_for_file: type=lint, implicit_dynamic_parameter, implicit_dynamic_type, implicit_dynamic_method, strict_raw_type
+
 part of 'main.dart';
 
 // **************************************************************************
 // JsonSerializableGenerator
 // **************************************************************************
 
-Entity _$EntityFromJson(Map<String, dynamic> json) => Entity(
+Entity _$EntityFromJson(Map json) => Entity(
       name: json['name'] as String,
       createdAt: json['createdAt'] == null
           ? const ServerTimestamp()
